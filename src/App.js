61918def6a1d3e0405d7b1305f957d6efaa4af1e
RENAN ALVES CARVALHO MENDES <renanalves23@outlook.com>
2021-05-23 14:56:45 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "./Counter";
+import { getUser } from "./redux/ducks/user";
 import "./styles.css";
 
 export default function App() {
+  const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.count);
   const voters = [
     "Anthony Sistilli ",
@@ -11,8 +13,16 @@ export default function App() {
     "Stephanie Foo",
     "Kevin Ma"
   ];
+
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
   return (
     <div className="App">
+      <h1>Hello, {user?.firstName}</h1>
       <h1>Redux made easy</h1>
       <h2> Total Votes: {count}</h2>
       {voters.map((voter, id) => (
